d39d4c5760e9ef32882fa54ce2df92d4a6692e0f
vladbabiuc <vlad_93@hotmail.it>
2017-06-09 09:43:12 +0200

    Creata AutoreRepository

---
+++ progetto-siw/src/main/java/it/uniroma3/repository/AutoreRepository.java
@@ -0,0 +1,22 @@
+package it.uniroma3.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.modello.Autore;
+
+public interface AutoreRepository extends CrudRepository <Autore, Long>{
+	
+	List<Autore> findByNome (String nome);
+	
+	List<Autore> findByCognome (String cognome);
+	
+	List<Autore> findByNazionalita (String nazionalita);
+	
+	List<Autore> findByDataNascita (Date dataNascita);
+	
+	List<Autore> findByDataMorte (Date dataMorte);
+
+}
