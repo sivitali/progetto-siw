5347957e0e9bde0aa15376a26bb8f2c16eae7044
vladbabiuc <vlad_93@hotmail.it>
2017-06-09 10:47:48 +0200

    Aggiunta AutoreController

---
+++ progetto-siw/src/main/java/it/uniroma3/controller/AutoreController.java
@@ -0,0 +1,42 @@
+package it.uniroma3.controller;
+
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.modello.Autore;
+import it.uniroma3.service.AutoreService;
+
+@Controller
+public class AutoreController {
+
+
+	@Autowired
+	private AutoreService autoreservice;
+
+	@GetMapping ("/autore")
+	public String showForm(Autore autore){
+		return "formAutore";
+	}
+
+	@PostMapping ("/autore")
+	public String checkAutoreInfo (@Valid @ModelAttribute Autore autore, BindingResult bindingResult, 
+			Model model){
+
+		if (bindingResult.hasErrors()) {                                                                              // se ci sono errori , ci rimanda alla form
+			return "formAutore";                                                          
+		}
+		else {                                                                                                          //model è un wrapper
+			model.addAttribute(autore);
+			autoreservice.add(autore); 
+		}
+		return "resultsAutore";               
+	}
+}
