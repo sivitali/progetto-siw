a2519de4ff9f2aef4f60c0f946b1ea2bd3111d93
sivitali <simonevitali1995@gmail.com>
2017-06-09 10:44:04 +0200

    Aggiunto OperaController

---
+++ progetto-siw/src/main/java/it/uniroma3/controller/OperaController.java
@@ -0,0 +1,38 @@
+package it.uniroma3.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.modello.Opera;
+import it.uniroma3.service.OperaService;
+
+@Controller
+public class OperaController {
+	
+	@Autowired
+	private OperaService operaService;
+	
+	@GetMapping("/opera")
+	public String showFormOpera(Opera opera){
+		return "formOpera";
+	}
+	
+	@PostMapping("/opera")
+	public String checkOperaInfo(@Valid @ModelAttribute Opera opera, BindingResult result, Model model){
+		if(result.hasErrors())
+			return "formOpera";
+		else{
+			model.addAttribute(opera);
+			this.operaService.add(opera);
+		}
+		return "resultsOpera";
+	}
+
+}
