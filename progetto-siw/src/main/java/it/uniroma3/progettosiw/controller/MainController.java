201c10e9e0d9396364ef3641453b12b6f9f37db4
vladbabiuc <vlad_93@hotmail.it>
2017-06-13 10:39:37 +0200

    Modifica al MainController

--- progetto-siw/src/main/java/it/uniroma3/progettosiw/controller/MainController.java
+++ progetto-siw/src/main/java/it/uniroma3/progettosiw/controller/MainController.java
@@ -1,8 +1,25 @@
 package it.uniroma3.progettosiw.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+// Controller per accedere alla pagina di login
 @Controller
 public class MainController {
+
+	// Login form
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	// Login form con errori
+	@RequestMapping("/login-error.html")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
+	}
 
 }
