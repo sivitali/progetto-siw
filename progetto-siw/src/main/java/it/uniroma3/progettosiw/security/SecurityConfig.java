4b926efd56af7eb3f0a4d95ee35ff8d1b0b99134
vladbabiuc <vlad_93@hotmail.it>
2017-06-13 11:32:17 +0200

    Aggiunte le credenziali d'accesso

--- progetto-siw/src/main/java/it/uniroma3/progettosiw/security/SecurityConfig.java
+++ progetto-siw/src/main/java/it/uniroma3/progettosiw/security/SecurityConfig.java
@@ -3,11 +3,16 @@ package it.uniroma3.progettosiw.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,10 +31,12 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
 
 		auth.jdbcAuthentication().dataSource(dataSource)
+		
+		.passwordEncoder(new BCryptPasswordEncoder())
 
-		.usersByUsernameQuery("select username,password, enabled from users where username=?")
+		.usersByUsernameQuery("SELECT username,password, 1 FROM users where username=?")
 
-		.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+		.authoritiesByUsernameQuery("SELECT username,authority, FROM authorities where username=?");
 
 	}
 
@@ -37,7 +44,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
 	@Override
 
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 
 		http
 		.authorizeRequests().antMatchers("/","/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
@@ -50,8 +57,14 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 		.logout()
 		.permitAll();
 		http.exceptionHandling().accessDeniedPage("/403");
+
 	}
-
-
-
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("admin").password("password").roles("USER","ADMIN").build());
+		return manager;
+	}
 }
+
+
