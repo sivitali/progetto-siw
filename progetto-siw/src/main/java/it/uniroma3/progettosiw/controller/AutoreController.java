75ea09aa1b7f51d73085abc7a453c8c1633dd7c8
sivitali <simonevitali1995@gmail.com>
2017-06-16 15:45:24 +0200

    Aggiunti Lista e Cancellazione di Autori

--- progetto-siw/src/main/java/it/uniroma3/progettosiw/controller/AutoreController.java
+++ progetto-siw/src/main/java/it/uniroma3/progettosiw/controller/AutoreController.java
@@ -9,7 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.uniroma3.progettosiw.model.Autore;
 import it.uniroma3.progettosiw.service.AutoreService;
@@ -30,13 +33,28 @@ public class AutoreController {
 	public String checkAutoreInfo (@Valid @ModelAttribute Autore autore, BindingResult bindingResult, 
 			Model model){
 
-		if (bindingResult.hasErrors()) {                                                                              // se ci sono errori , ci rimanda alla form
+		if (bindingResult.hasErrors()) {
 			return "formAutore";                                                          
 		}
-		else {                                                                                                          //model è un wrapper
+		else {
 			model.addAttribute(autore);
 			autoreService.add(autore); 
 		}
 		return "datiAutore";               
+	}
+	@GetMapping("/autori")
+	public String getAutori(Model model){
+		model.addAttribute("autori",this.autoreService.findAll());
+		return "listaAutori";
+	}
+	@RequestMapping("/autori/{id}")
+	public String getAutore(@PathVariable Long id,Model model){
+		model.addAttribute("autore",this.autoreService.findById(id));
+		return "datiAutore";
+	}
+	@RequestMapping(value="/autori/{id}",method=RequestMethod.DELETE)
+	public String deleteAutore(@PathVariable Long id){
+		this.autoreService.deleteById(id);
+		return "redirect:/autori";
 	}
 }
