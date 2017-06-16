676862fe4548473e8d2af17256da40cd16c66621
sivitali <simonevitali1995@gmail.com>
2017-06-16 11:52:40 +0200

    Risolto problema di autenticazione

--- progetto-siw/src/main/java/it/uniroma3/progettosiw/security/SecurityConfig.java
+++ progetto-siw/src/main/java/it/uniroma3/progettosiw/security/SecurityConfig.java
@@ -1,62 +1,37 @@
 package it.uniroma3.progettosiw.security;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-
-	private DataSource dataSource;
-
-
-
-	@Override
-
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-
-
-		auth.jdbcAuthentication().dataSource(dataSource)
-		
-		.passwordEncoder(new BCryptPasswordEncoder())
-
-		.usersByUsernameQuery("SELECT username,password, 1 FROM users where username=?")
-
-		.authoritiesByUsernameQuery("SELECT username,authority, FROM authorities where username=?");
-
-	}
-
-
-
 	@Override
 
 	public void configure(HttpSecurity http) throws Exception {
 
 		http
-		.authorizeRequests().antMatchers("/","/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
-		.anyRequest().authenticated()     
-		.and()
+		.authorizeRequests()
+			.antMatchers("/","/galleria").permitAll()
+			.antMatchers("/opera","autore").hasRole("ADMIN")
+		.anyRequest()
+			.authenticated()
+			.and()
 		.formLogin()
-		.loginPage("/login")
-		.permitAll()
-		.and()
+			.loginPage("/login")
+			.permitAll()
+			.and()
 		.logout()
-		.permitAll();
+			.permitAll();
 		http.exceptionHandling().accessDeniedPage("/403");
+	
 
 	}
 	@Bean
@@ -66,5 +41,3 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 		return manager;
 	}
 }
-
-
---
+++ progetto-siw/src/main/java/it/uniroma3/progettosiw/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package it.uniroma3.progettosiw.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	public SecurityWebApplicationInitializer() {
+		super(SecurityConfig.class);
+	}
+}
